331bb2f328524d0a1654c43e92ae9101d8674766
JhonedwardParra <jhonp630@gmail.com>
2018-08-13 21:37:06 -0500

    Index Admin

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<?php include('views/front/templates/head.php'); ?>	
+<div class="row">	
+    <aside>
+    	<div class="usuario">
+    		<img class="usu" src=" http://localhost/Indexadmin/public/images/usuario login.png">
+    		<h3>Edward Parra</h3>
+    	</div>
+    	<h4>Indicadores</h4>
+    	<nav>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    	</nav>
+    	<div class="usuariofoot"></div>
+	</aside>
+<section class="col2">
+	<h2>Puntuacion</h2>
+
+	    <article class="estad1">
+	    	<img class="estad" src=" http://localhost/Indexadmin/public/images/menu.png">
+	    </article>
+	    <article class="estad2">
+	    	<img class="estad" src=" http://localhost/Indexadmin/public/images/signo.png">
+	    </article>
+	    <article class="estad3">
+	    	<img class="estad" src=" http://localhost/Indexadmin/public/images/dialogo.png">
+	    	
+	    </article>
+	    <article class="estad4">
+	    	<img class="estad" src=" http://localhost/Indexadmin/public/images/usuario.png">
+	    </article>
+	    <div class="grafica">
+	    <h2>CPU USAGE (%)</h2> 	
+	    <img class="imagen" src=" http://localhost/Indexadmin/public/images/grafica.jpg">
+	    </div>
+	    <article class="caja1">
+	    	<img class="imacaja1" src=" http://localhost/Indexadmin/public/images/esdtad.png">
+	    </article>
+	    <article class="caja2">
+
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		<a href="#">HOME</a><br>
+    		
+        	
+	    </article>
+	    <article class="caja3"></article>
+</section>	 
+	    
+</div>
+   
+</div>
+<?php include('views/front/templates/foot.php'); ?>
---
+++ views/front/templates/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="http://localhost/Indexadmin/public/css\style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+	<header>
+		<h1>ADMINBSB</h1>
+		<div class="buscar">
+			<img class="redes" src=" http://localhost/Indexadmin/public/images/lupa.png">
+			<img class="redes" src=" http://localhost/Indexadmin/public/images/facebook.png">
+			<img class="redes" src=" http://localhost/Indexadmin/public/images/instagram.png">
+			<img class="redes" src=" http://localhost/Indexadmin/public/images/twiter.png">
+		</div>
+	</header>
